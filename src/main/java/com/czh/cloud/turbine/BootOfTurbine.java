c6ba36643d7c3958b4f25a3d06be1253bc0aced8
chenzhehao <chenzhehao2008666@icloud.com>
2018-12-22 23:52:24 +0800

    服务增加测试接口，配置修改，解决引入web报错hibernate问题，jboss jar包问题

--- src/main/java/com/czh/cloud/turbine/BootOfTurbine.java
+++ src/main/java/com/czh/cloud/turbine/BootOfTurbine.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Hello world!
@@ -15,16 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 @EnableHystrixDashboard
 @EnableTurbine // http://127.0.0.1:12086/hystrix
-public class App {
+public class BootOfTurbine {
     public static void main(String[] args) throws Exception {
         String os = System.getProperty("os.name").toLowerCase();
         if (os.contains("windows")) {
             System.setProperty("log.path", "D:/chenzhehao/workspace/czh");
         } else {
-            System.setProperty("log.path", "/opt");
+            System.setProperty("log.path", "/Users/chenzhehao/Documents/logs/springcloud");
         }
         System.setProperty("context.name", "turbine-server");
 
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(BootOfTurbine.class, args);
+    }
+
+    @RequestMapping("/")
+    public Object test() {
+        return "success";
     }
 }
